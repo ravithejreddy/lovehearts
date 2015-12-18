4e37f522f0d334002abd95c8352145e9551f3bc6
rc <ravithejreddy@gmail.com>
2015-12-18 12:51:00 +1100

    some styling to the app

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,18 +4,21 @@ class PagesController < ApplicationController
   end
 
 
-  # 
-  # def twitter
-  #   @client ||= Twitter::REST::Client.new do |config|
-  #     config.consumer_key        = ENV["CONSUMER_KEY"]
-  #     config.consumer_secret     = ENV["CONSUMER_SECRET"]
-  #   end
-  # end
+
+  def twitter
+    @client ||= Twitter::REST::Client.new do |config|
+      config.consumer_key        = ENV["CONSUMER_KEY"]
+      config.consumer_secret     = ENV["CONSUMER_SECRET"]
+    end
+  end
   #
   #
   # def feed
   #
-  #   @feed = client.user_timeline("TechCrunch", count:100 , exclude_replies: true, include_rts: false)
+  # https:/api.twitter.com/1.1/search/tweets.json?q=starwars&lang=en&count=100&favourites_count=500&retweets_count=500
+  #
+
+  # @feed = client.user_timeline("TechCrunch", count:100 , exclude_replies: true, include_rts: false)
   #
   # end
 
