39a85942f186db8627e4c3745e2fc456a5934ac1
rc <ravithejreddy@gmail.com>
2015-12-18 11:42:34 +1100

    readme file updated

--- lib/Twitter_Api.rb
+++ lib/Twitter_Api.rb
@@ -1,20 +1,20 @@
-class TwitterApi
-
-  def self.feed
-
-    client.user_timeline("TechCrunch", count:100 , exclude_replies: true, include_rts: false)
-
-  end
-
-
-
-  def self.client
-  @client ||= Twitter::REST::Client.new do |config|
-  # config.consumer_key        = "QEelJ7Lwzho29DAspVE4Y9GtU"
-  # config.consumer_secret     = "qPCCOwfaHCt1uwHGy1I2UVjPmZ4KKaFEgpTsdW6jlHent9Bgex"
-  config.consumer_key        = ENV["CONSUMER_KEY"]
-  config.consumer_secret     = ENV["CONSUMER_SECRET"]
-end
-end
-
-end
+# class TwitterApi
+#
+#   def self.feed
+#
+#     client.user_timeline("TechCrunch", count:100 , exclude_replies: true, include_rts: false)
+#
+#   end
+#
+#
+#
+#   def self.client
+#   @client ||= Twitter::REST::Client.new do |config|
+#   # config.consumer_key        = "QEelJ7Lwzho29DAspVE4Y9GtU"
+#   # config.consumer_secret     = "qPCCOwfaHCt1uwHGy1I2UVjPmZ4KKaFEgpTsdW6jlHent9Bgex"
+#   config.consumer_key        = ENV["CONSUMER_KEY"]
+#   config.consumer_secret     = ENV["CONSUMER_SECRET"]
+# end
+# end
+#
+# end
