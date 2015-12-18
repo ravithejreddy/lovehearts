3c9301d3e2c91a5209da72f7da50cb04f09ca35f
rc <ravithejreddy@gmail.com>
2015-12-18 11:53:03 +1100

    few changes to pages_controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,20 +4,20 @@ class PagesController < ApplicationController
   end
 
 
-
-  def twitter
-    @client ||= Twitter::REST::Client.new do |config|
-      config.consumer_key        = ENV["CONSUMER_KEY"]
-      config.consumer_secret     = ENV["CONSUMER_SECRET"]
-    end
-  end
-
-
-  def feed
-
-    @feed = client.user_timeline("TechCrunch", count:100 , exclude_replies: true, include_rts: false)
-
-  end
+  # 
+  # def twitter
+  #   @client ||= Twitter::REST::Client.new do |config|
+  #     config.consumer_key        = ENV["CONSUMER_KEY"]
+  #     config.consumer_secret     = ENV["CONSUMER_SECRET"]
+  #   end
+  # end
+  #
+  #
+  # def feed
+  #
+  #   @feed = client.user_timeline("TechCrunch", count:100 , exclude_replies: true, include_rts: false)
+  #
+  # end
 
 
 end
